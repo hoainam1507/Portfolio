2331382024038f7f88e8eab270b118aaced63b7d
hoai nam <hoainamngo9@gmail.com>
2023-12-01 23:04:42 +0700

    Updates

---
+++ precache-manifest.0bd57e237e0adcb800d8cbbb1647f0f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13a5d8fbc72371a0d31f4789b30342e9",
+    "url": "/hoainam1507/Portfolio.git/index.html"
+  },
+  {
+    "revision": "d7b87f5e03a62aa16ad0",
+    "url": "/hoainam1507/Portfolio.git/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "4d42c5157cc081ef989a",
+    "url": "/hoainam1507/Portfolio.git/static/css/main.b90b03de.chunk.css"
+  },
+  {
+    "revision": "d7b87f5e03a62aa16ad0",
+    "url": "/hoainam1507/Portfolio.git/static/js/2.a4f245ca.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/hoainam1507/Portfolio.git/static/js/2.a4f245ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d42c5157cc081ef989a",
+    "url": "/hoainam1507/Portfolio.git/static/js/main.1ac7cef6.chunk.js"
+  },
+  {
+    "revision": "d59904eec251d71b7745",
+    "url": "/hoainam1507/Portfolio.git/static/js/runtime-main.5a420614.js"
+  }
+]);
